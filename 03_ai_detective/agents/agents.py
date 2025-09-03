033df10fd9ccc040910ef5d4c8923bf10b698a38
TeaAlc <no@mail.com>
2025-09-03 14:54:25 +0200

    chore: added get_custom_chain_agent

--- 03_ai_detective/agents/agents.py
+++ 03_ai_detective/agents/agents.py
@@ -1,5 +1,6 @@
 from langchain.agents import initialize_agent, AgentType
 from langchain_core.language_models import BaseChatModel
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langgraph.prebuilt import create_react_agent
 
@@ -13,24 +14,29 @@ class Agent:
         self.model = model
         self.tools = tools
 
+        self.model_w_tools = self.model.bind_tools(self.tools)
+
         self._react_agent = None
         self._langchain_agent = None
+        self._custom_agent = None
+
+    def _build_prompt(self) -> ChatPromptTemplate:
+        return ChatPromptTemplate.from_messages([
+            (
+                "system",
+                f"**YOUR NAME IS {self.name}**\n\n{self.system_prompt}"
+            ),
+            ("user", "{input}"),
+            ("assistant", "{agent_scratchpad}")
+        ])
 
     def get_langchain_agent(self):
         if self._langchain_agent is None:
-            prompt = ChatPromptTemplate.from_messages([
-                (
-                    "system",
-                    f"**YOUR NAME IS {self.name}**\n\n{self.system_prompt}"
-                ),
-                ("user", "{input}"),
-                ("assistant", "{agent_scratchpad}")
-            ])
             self._langchain_agent = initialize_agent(
                 llm=self.model,
                 tools=self.tools,
                 agent=AgentType.OPENAI_FUNCTIONS,
-                agent_kwargs={"prompt": prompt},
+                agent_kwargs={"prompt": self._build_prompt()},
                 verbose=True
             )
         return self._langchain_agent
@@ -40,8 +46,14 @@ class Agent:
             self._react_agent = create_react_agent(
                 model=self.model,
                 tools=self.tools,
-                prompt= f"**YOUR NAME IS {self.name}**\n\n{self.system_prompt}",
+                prompt= self._build_prompt(),
                 name=self.name)
+
+    def get_custom_chain_agent(self):
+        if self._custom_agent is None:
+            chain = self._build_prompt() | self.model_w_tools | StrOutputParser()
+            self._custom_agent = chain
+        return self._custom_agent
 
     @classmethod
     def create(cls, name:str, sys_prompt_path:str, model:BaseChatModel) -> "Agent":
