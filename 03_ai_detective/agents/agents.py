a585eb2692e10711da76ca6914d4bf36a315654a
TeaAlc <no@mail.com>
2025-09-03 12:02:22 +0200

    chore: changed Agent to being able to return a react agent

--- 03_ai_detective/agents/agents.py
+++ 03_ai_detective/agents/agents.py
@@ -1,15 +1,25 @@
-from langchain_core.language_models import BaseChatModel
+from langgraph.prebuilt import create_react_agent
 
 from helper import read_text_file
 
 
 class Agent:
-    def __init__(self, name:str, system_prompt:str, llm:BaseChatModel):
+    def __init__(self, name:str, system_prompt:str, model:str):
         self.name = name
         self.system_prompt = system_prompt
-        self.llm = llm
+        self.model = model
+
+        self._react_agent = None
+
+    def get_react_agent(self):
+        if self._react_agent is None:
+            self._react_agent = create_react_agent(
+                model=self.model,
+                tools=[],
+                prompt= f"**YOUR NAME IS {self.name}**\n\n{self.system_prompt}",
+                name=self.name)
 
     @classmethod
-    def create(name:str, sys_prompt_path:str, llm:BaseChatModel) -> "Agent":
+    def create(cls, name:str, sys_prompt_path:str, model:str) -> "Agent":
         sys_prompt = read_text_file(sys_prompt_path)
-        return Agent(name, sys_prompt, llm)
+        return Agent(name, sys_prompt, model)
